6cc02bb1ba22a48851257fe50a8ed6879511c12b
Kazım Etiksan <kazim@pikselmutfak.com>
2022-01-04 14:49:55 +0300

    first

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+const appInsights = require('applicationinsights')
+
+appInsights
+    .setAutoDependencyCorrelation(true)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true)
+    .setUseDiskRetryCaching(true)
+    .setSendLiveMetrics(false)
+    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
+    .start()
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/api/hello', async (req, res) => {
+
+    const somekey = process.env.APPSETTING_somekey
+    res.send('somekey='+somekey)
+})
+
+app.get('/api/merhaba', (req, res) => {
+
+    res.send('Merhaba Dünya')
+})
+
+app.post('/api/name', (req, res) => {
+
+    const body = _.pick(req.body, ['firstName','lastName'])
+    console.log(body)
+    res.send('Hello '+body.firstName+' '+body.lastName)
+})
+
+app.listen(8080, () => {
+    console.log('app server is running')
+})
